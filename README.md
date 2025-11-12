2d609abb917860875a34439fb4417e121c8544d1
JoaoVictor019 <jvictorvendito@gmail.com>
2025-11-12 18:36:44 -0300

    a

---
+++ README.md
@@ -0,0 +1,122 @@
+
+
+📦 Instale os requisitos do projeto:
+# VaiJunto
+
+## Descrição do Projeto
+O **VaiJunto** é um aplicativo mobile desenvolvido para facilitar a conexão entre estudantes universitários que desejam compartilhar caronas. A ideia central do projeto é permitir que os usuários ofereçam ou encontrem caronas de maneira prática e segura, economizando tempo e recursos. Com o app, os estudantes podem buscar caronas disponíveis, oferecer vagas em seus veículos e se conectar com outros colegas que seguem rotas semelhantes.
+
+## Funcionalidades
+- **Buscar Carona**: O usuário pode pesquisar caronas disponíveis para uma determinada localização e horário.
+- **Oferecer Carona**: Os usuários que desejam oferecer caronas podem cadastrar rotas, horários e quantas vagas estão disponíveis.
+- **Seleção de Vagas Disponíveis**: Função interativa para selecionar o número de vagas no carro.
+  
+## Ferramentas e Linguagens Utilizadas
+- **React Native**: Framework utilizado para desenvolvimento de aplicativos móveis multiplataforma.
+- **Expo**: Ferramenta utilizada para facilitar o desenvolvimento e o deploy do app React Native.
+- **JavaScript**: Linguagem de programação utilizada no desenvolvimento do aplicativo.
+- **React Navigation**: Biblioteca para gerenciar as rotas e navegação entre as telas do aplicativo.
+- **@react-native-picker/picker**: Utilizado para implementar seleções interativas (dropdowns).
+
+## Instalação de Dependências e Configuração
+
+### Pré-requisitos
+- **Node.js**: Plataforma para rodar JavaScript fora do navegador, essencial para o ambiente de desenvolvimento React Native.
+- **Expo CLI**: Interface de linha de comando para desenvolvimento e execução de projetos Expo.
+
+### Instalação do Node.js
+Para instalar o **Node.js**, siga o tutorial disponível no site oficial [Node.js](https://nodejs.org).
+Node.JS sempre a versão mais recente
+React Native sempre a versão mais recente
+
+### Instalação do Expo CLI
+Após instalar o Node.js, instale o **Expo CLI** executando o seguinte comando no terminal:
+- **npm install -g expo-cli**
+
+### Dependências do Projeto
+No diretório do projeto, execute o seguinte comando para instalar todas as dependências necessárias, como React Navigation e o Picker:
+- **npm install**
+
+
+### Instalação das Dependências Individuais
+
+1. **React Navigation**: Para instalar o sistema de navegação do React Native, use o seguinte comando:
+- **npm install @react-navigation/native**
+
+2. **React Native Screens e React Native Safe Area Context**:
+- **npm install react-native-screens react-native-safe-area-context**
+
+3. **Picker**: Instale a biblioteca de seleção interativa:
+- **npm install @react-native-picker/picker**
+
+4. **React Native Calendars**:
+- **npm install react-native-calendars**
+
+5. **Back-end requirements**:
+No diretório 'django_backend', siga os passos:
+- Crie um ambiente virtual com **python -m venv venv**.
+
+- Ative o ambiente virtual com **venv\Scripts\activate** (Windows) ou **source venv/bin/activate** (MacOS/Linux)
+
+- Com o ambiente virtual ativado, rode **pip install -r requirements.txt**
+
+
+
+### Rodando o Projeto
+Após instalar todas as dependências, você pode iniciar o projeto com o comando:
+- **npm start**
+
+### Verificando o Banco de dados
+Execute o comando **python manage.py runserver "IP LOCAL":800**
+Substitua IP LOCAL pelo seu endereço IP (pode ser consultado executando "ipconfig" no terminal)
+
+Isso abrirá o **Expo Dev Tools** no seu navegador. Com ele, você poderá executar o aplicativo no emulador, no seu dispositivo móvel ou no simulador de iOS/Android.
+
+## Sobre Expo e Node.js
+- **Expo**: Uma plataforma para desenvolvimento de aplicativos em React Native, que facilita o processo de desenvolvimento, permitindo que você teste seu aplicativo diretamente em dispositivos móveis com o Expo Go.
+- **Node.js**: Uma plataforma que executa JavaScript no servidor, essencial para a instalação e gerenciamento de pacotes com o NPM (Node Package Manager) e para o desenvolvimento de projetos em React Native.
+
+Com essas instruções, você poderá instalar e configurar o ambiente de desenvolvimento do VaiJunto! e rodar o aplicativo no Expo!
+
+Instruções para instalação em um computador com Windows 11
+
+Caso não tenha o chocolate instalado, inicie o preparo do sistema abrindo um término do powershell com privilégio de administrador
+
+PS> Set-ExecutionPolicy AllSigned
+
+PS> Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+PS> choco --version
+Com o chocolate instalado, continuamos com a instalação dos requisitos do projeto
+
+PS> choco install nodejs-lts -y
+
+PS> choco install openjdk17 -y
+
+PS> choco install nvm -y
+
+🚀 Execute o projeto:
+npx expo start
+
+Telas do projeto
+Capture todas as telas do projeto e identifique-as
+
+Tela 1: login
+
+Tela 2: criação de usuário
+
+Tela 3: recuperação de senha
+
+Tela 4: tela inicial
+
+...e assim por diante
+
+
+
+
+
+
+
+
+
+
---
+++ frontend/react_native_app/screens/cadastro.js
@@ -0,0 +1,236 @@
+
+import React, { useEffect, useState } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Alert,
+  SafeAreaView,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  ActivityIndicator
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+const CadastroScreen = () => {
+  const [nomeCompleto, setNomeCompleto] = useState('');
+  const [cpf, setCpf] = useState('');
+  const [email, setEmail] = useState('');
+  const [telefone, setTelefone] = useState('');
+  const [matricula, setMatricula] = useState('');
+  const [curso, setCurso] = useState('');
+  const [senha, setSenha] = useState('');
+  const [confirmarSenha, setConfirmarSenha] = useState('');
+  const [serverIp, setServerIp] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigation = useNavigation();
+
+  const validarCampos = () => {
+    if (
+      !nomeCompleto.trim() ||
+      !cpf.trim() ||
+      !email.trim() ||
+      !telefone.trim() ||
+      !matricula.trim() ||
+      !curso.trim() ||
+      !senha.trim() ||
+      !confirmarSenha.trim()
+    ) {
+      Alert.alert('Atenção', 'Por favor, preencha todos os campos.');
+      return false;
+    }
+
+    if (cpf.length !== 11 || !/^\d+$/.test(cpf)) {
+      Alert.alert('Atenção', 'CPF inválido. Deve conter 11 dígitos numéricos.');
+      return false;
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      Alert.alert('Atenção', 'E-mail inválido.');
+      return false;
+    }
+
+    if (senha !== confirmarSenha) {
+      Alert.alert('Erro', 'As senhas não coincidem!');
+      return false;
+    }
+
+    return true;
+  };
+
+  const handleCadastroUsuario = async () => {
+    if (!validarCampos()) return;
+
+    const dadosCadastro = {
+      nome: nomeCompleto,
+      cpf: cpf,
+      email: email,
+      telefone: telefone,
+      matricula: matricula,
+      curso: curso,
+      senha: senha,
+    };
+
+    setIsLoading(true);
+    try {
+      const response = await fetch(`http://172.16.15.23/api/cadastrar_usuario/`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(dadosCadastro),
+      });
+
+      const responseText = await response.text();
+      console.log('Status da resposta:', response.status);
+      console.log('Corpo da resposta:', responseText);
+
+      if (response.ok) {
+        Alert.alert(
+          'Sucesso',
+          'Usuário cadastrado com sucesso!',
+          [
+            {
+              text: 'OK',
+              onPress: () => {
+                setTimeout(() => {
+                  navigation.navigate('Home');
+                }, 100);
+              },
+            },
+          ],
+          { cancelable: false }
+        );
+      } else {
+        Alert.alert('Erro', 'Não foi possível cadastrar o usuário.');
+      }
+    } catch (error) {
+      console.error('Erro de conexão:', error);
+      Alert.alert('Erro', 'Falha na conexão com o servidor.');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const getPublicIp = async () => {
+      try {
+        const response = await fetch('https://api.ipify.org?format=json');
+        const data = await response.json();
+        setServerIp(data.ip);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getPublicIp();
+  }, []);
+
+  return (
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      keyboardVerticalOffset={80}
+    >
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <SafeAreaView style={styles.innerContainer}>
+          <TouchableOpacity
+            style={styles.backButton}
+            onPress={() => navigation.navigate('Home')}
+          >
+            <Text style={styles.backText}>← Voltar</Text>
+          </TouchableOpacity>
+
+          <Text style={styles.title}>VaiJunto?</Text>
+
+          <TextInput style={styles.input} placeholder="Nome completo" placeholderTextColor="#ccc" value={nomeCompleto} onChangeText={setNomeCompleto} />
+          <TextInput style={styles.input} placeholder="CPF" placeholderTextColor="#ccc" value={cpf} onChangeText={setCpf} keyboardType="numeric" />
+          <TextInput style={styles.input} placeholder="Email" placeholderTextColor="#ccc" value={email} onChangeText={setEmail} keyboardType="email-address" />
+          <TextInput style={styles.input} placeholder="Telefone" placeholderTextColor="#ccc" value={telefone} onChangeText={setTelefone} keyboardType="phone-pad" />
+          <TextInput style={styles.input} placeholder="Matrícula (RA)" placeholderTextColor="#ccc" value={matricula} onChangeText={setMatricula} keyboardType="numeric" />
+          <TextInput style={styles.input} placeholder="Curso" placeholderTextColor="#ccc" value={curso} onChangeText={setCurso} />
+          <TextInput style={styles.input} placeholder="Senha" placeholderTextColor="#ccc" value={senha} onChangeText={setSenha} secureTextEntry />
+          <TextInput style={styles.input} placeholder="Confirmar Senha" placeholderTextColor="#ccc" value={confirmarSenha} onChangeText={setConfirmarSenha} secureTextEntry />
+
+          <TouchableOpacity
+            style={[styles.button, isLoading && styles.buttonDisabled]}
+            onPress={handleCadastroUsuario}
+            disabled={isLoading}
+          >
+            {isLoading ? (
+              <ActivityIndicator size="small" color="#fff" />
+            ) : (
+              <Text style={styles.buttonText}>Cadastrar</Text>
+            )}
+          </TouchableOpacity>
+        </SafeAreaView>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#12023d',
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+  },
+  backButton: {
+    position: 'absolute',
+    left: 16,
+    top: Platform.OS === 'ios' ? 50 : 20,
+    zIndex: 10,
+    padding: 6,
+  },
+  backText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  title: {
+    marginTop: 40,
+    fontSize: 48,
+    fontWeight: 'bold',
+    marginBottom: 16,
+    textAlign: 'center',
+    color: '#fff',
+  },
+  input: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#fff',
+    margin: 10,
+    height: 50,
+    borderColor: '#4f0466',
+    borderWidth: 4,
+    marginBottom: 12,
+    paddingHorizontal: 10,
+    borderRadius: 24,
+    fontWeight: 'bold',
+  },
+  button: {
+    backgroundColor: '#ff4800',
+    borderWidth: 2,
+    borderRadius: 24,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    alignItems: 'center',
+    marginTop: 20,
+    width: 300,
+    alignSelf: 'center',
+  },
+  buttonDisabled: {
+    opacity: 0.7,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
+
+export default CadastroScreen;
