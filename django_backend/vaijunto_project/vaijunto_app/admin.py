149e3fe59034eaff4faf44c3da2c24541fb1d0ea
JoaoVictor019 <jvictorvendito@gmail.com>
2025-11-12 19:30:45 -0300

    Remove django_backend

--- django_backend/vaijunto_project/vaijunto_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Carona, Viagem, Cadastrar_usuario
-
-# Registra os modelos para aparecerem no painel de administração do Django
-admin.site.register(Carona)
-admin.site.register(Viagem)
-admin.site.register(Cadastrar_usuario)
